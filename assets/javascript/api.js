e47ae24d7630d659d6314794c5769c03ab7a63ff
Ryan <rjlafazan@gmail.com>
2018-02-27 13:50:34 -0800

    working buttons from array and gif generation

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,28 +1,96 @@
-$("#cat-button").on("click", function() {
+searchItemArray = [
+  'judo',
+  'muay thai',
+  'hiking',
+  'camping',
+  'whiskey',
+  'beer'
+];
 
-      //
-      var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
+function buttonMake() {
+  $('#buttonSpot').empty();
 
-      //
+  for (var i = 0; i < searchItemArray.length; i++) {
+    searchItemArray[i]
+    var a = $('<button>');
+    a.addClass('testClass');
+    a.attr('data-item', searchItemArray[i]);
+    a.text(searchItemArray[i]);
+    $('#buttonSpot').append(a);
+  };
+};
+buttonMake();
+
+
+$(".testClass").on("click", function() {
+  var getSearchItem = $(this).attr('data-item')
+  console.log(this);
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + getSearchItem + "&api_key=sQRBNLJFYJUqADVUULepENnHFjGwnJtQ&limit=10";
+
+
+      // AJAX request to grab giphy api data
       $.ajax({
         url: queryURL,
         method: "GET"
       })
 
-      //
+      // Use the response from the giphy api to do the following
       .then(function(response) {
 
-        //
-        var imageUrl = response.data.image_original_url;
+        // check to see what the queryURL and response are pulling
+        console.log(queryURL);
+        console.log(response);
 
-        //
-        var catImage = $("<img>");
+        // store the data from the response as results
+        var results = response.data;
 
-        //
-        catImage.attr("src", imageUrl);
-        catImage.attr("alt", "cat image");
+        // loop through the result items and creating some tags and grabbing specific results
+        for (var i = 0; i < results.length; i++) {
 
-        //
-        $("#images").prepend(catImage);
+          var gifStuff = $('<div>');
+
+          var p = $('<p>').text('Rating: ' + results[i].rating);
+
+          var gifImage = $('<img>');
+
+          gifImage.attr('src', results[i].images.fixed_height.url);
+
+          gifStuff.append(p);
+          gifStuff.append(gifImage);
+
+          $('#images').prepend(gifStuff);
+
+        };
+
+
+        // for (var i = 0; i < results.length; i++) {
+
+        //   var gifStuff = $('<div>');
+        //   gifStuff.addClass('carousel-item');
+
+
+        //   // var p = $('<p>').text('Rating: ' + results[i].rating);
+
+        //   var gifImage = $('<img>');
+
+        //   gifImage.attr('src', results[i].images.fixed_height.url);
+        //   gifImage.addClass('d-block w-100');
+
+        //   // gifStuff.append(p);
+        //   gifStuff.append(gifImage);
+
+        //   $('#carousel-images').prepend(gifStuff);
+
+        // };
+      
       });
 });
+
+
+
+
+
+
+
+
